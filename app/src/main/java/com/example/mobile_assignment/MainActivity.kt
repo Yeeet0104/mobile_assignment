dd0b42ac7cd8e13f7951d438cdb5c2123f5277f7
yaprouxin <yaprouxin@gmail.com>
2023-05-02 23:32:11 +0800

    bottom nav update pt1

--- app/src/main/java/com/example/mobile_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assignment/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.mobile_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("testing")
-        println("testing2")
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+//        val appBarConfiguration = AppBarConfiguration(setOf(R.id.workoutFragment,
+//            R.id.waterTrackerFragment, R.id.nutritionTrackerFragment, R.id.nutritionTrackerFragment,
+//            R.id.profileFragment))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
