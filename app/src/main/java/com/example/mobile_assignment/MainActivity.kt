7c2ff2dd98927101b53efd680162e0f9046ed5dd
WeiHong <tanweihong0104@gmail.com>
2023-05-02 21:28:38 +0800

    assignment v0.0.3

--- app/src/main/java/com/example/mobile_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assignment/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("testing")
         println("testing2")
+        println("testing3")
     }
 }
