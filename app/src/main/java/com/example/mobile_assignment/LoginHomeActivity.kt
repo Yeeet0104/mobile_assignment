fcfd35d4945de7fe17dd7bd44b670660fe4e36ba
ChuaSY <shiyingchua0614@gmail.com>
2023-05-07 19:17:57 +0800

    change LoginHomeActivity.kt error. but not sure can or not

--- app/src/main/java/com/example/mobile_assignment/LoginHomeActivity.kt
+++ app/src/main/java/com/example/mobile_assignment/LoginHomeActivity.kt
@@ -1,13 +1,18 @@
 package com.example.mobile_assignment
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.mobile_assignment.databinding.ActivityLoginHomeBinding
 
 class LoginHomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: LoginHomeActivity
+    private lateinit var binding: ActivityLoginHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,12 @@ class LoginHomeActivity : AppCompatActivity() {
         binding.buttonLogout.setOnClickListener {
             startActivity(Intent(this, LoginActiivty::class.java))
         }
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return binding.root
     }
 }
