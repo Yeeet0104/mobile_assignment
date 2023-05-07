bc1f97e7f216ed9783861072de0a74983bed0a3b
WeiHong <86936657+Yeeet0104@users.noreply.github.com>
2023-05-08 00:36:12 +0800

    Revert "Chua"

--- app/src/main/java/com/example/mobile_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assignment/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mobile_assignment
 
 import android.app.Notification.Action
-import android.content.Intent
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,15 +12,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI
-import com.example.mobile_assignment.databinding.ActivityLoginMainBinding
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNav: BottomNavigationView
-    private lateinit var navController: NavController
+    private lateinit var bottomNav:BottomNavigationView
+    private lateinit var navController:NavController
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityLoginMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,47 +29,37 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setupWithNavController(navController)
 //        setupBottomNavigation()
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+
 
         var toolbar = findViewById<Toolbar>(R.id.toolbarHeader)
         setSupportActionBar(toolbar)
 
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        val navigation_view = findViewById<NavigationView>(R.id.nav_view)
-        NavigationUI.setupWithNavController(navigation_view, navController)
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.workoutFragment,
-//                R.id.waterTrackerFragment,
-//                R.id.nutritionTrackerFragment,
-//                R.id.sleepTrackerFragment,
-//                R.id.profileFragment
-//            )
-//        )
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.workoutFragment,
+                R.id.waterTrackerFragment,
+                R.id.nutritionTrackerFragment,
+                R.id.sleepTrackerFragment,
+                R.id.profileFragment
+            ),
+            drawerLayout
+        )
+        NavigationUI.setupActionBarWithNavController(this,navController, appBarConfiguration)
 
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-
-        binding = ActivityLoginMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.buttonLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActiivty::class.java))
-        }
-        binding.buttonRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navigationView,navController)
 
 
     }
-
     override fun onSupportNavigateUp(): Boolean {
         //return navController.navigateUp()
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
 
     }
 
     private fun setupBottomNavigation() {
         bottomNav.setupWithNavController(navController)
     }
+
 }
