66207c386b149f90f3cb8792f62821d41f6cacca
b1ank3r <75467178+b1ank3r@users.noreply.github.com>
2023-05-08 18:25:34 +0800

    Saving Layout on Github. Check if I edited other people fragment.

---
+++ app/src/main/java/com/example/mobile_assignment/NutritionHistoryFragment.kt
@@ -0,0 +1,30 @@
+package com.example.mobile_assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.DialogFragment
+
+
+class NutritionOptionFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calories_option, container, false)
+
+
+    }
+
+}
