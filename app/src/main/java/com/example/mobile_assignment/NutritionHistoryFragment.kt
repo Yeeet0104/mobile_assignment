a2c0094231a2bd7c12d2e982a68e527275821cb5
b1ank3r <75467178+b1ank3r@users.noreply.github.com>
2023-05-09 18:43:43 +0800

    Commit Layout UI to prevent data loss

---
+++ app/src/main/java/com/example/mobile_assignment/NutritionHistoryFragment.kt
@@ -0,0 +1,34 @@
+package com.example.mobile_assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.SearchView
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.Navigation
+
+
+class NutritionHistoryFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calories_history, container, false)
+
+
+
+    }
+
+}
