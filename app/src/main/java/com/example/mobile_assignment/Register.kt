f0e1d810879826f5a6e22d6086cae4274062f167
ChuaSY <shiyingchua0614@gmail.com>
2023-05-09 15:11:33 +0800

    9/5/2023 No Update

--- app/src/main/java/com/example/mobile_assignment/Register.kt
+++ app/src/main/java/com/example/mobile_assignment/Register.kt
@@ -9,6 +9,7 @@ class Register : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
